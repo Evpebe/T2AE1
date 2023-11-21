1b81083a2475b09f8319b401225de8d305a5bee2
Evpebe <evpebe@floridauniversitaria.es>
2023-11-21 12:50:28 +0100

    archivos

---
+++ T2AE1/public/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Examen Eva Perona</title>
+        
+    </head>
+    <body>
+        
+            <?php
+                echo "<h1>EXAMEN Eva Perona</h1>";
+
+                $conn = mysqli_connect('database', 'root', 'root', "mamoro");
+                $query = 'SELECT * FROM persona';
+                $result = mysqli_query($conn, $query);
+
+                echo '<table class="table table-striped">';
+                echo 'ID NOMBRE ESTILO';
+                while($value = $result->fetch_array(MYSQLI_ASSOC)){
+                    echo '<tr>';
+                    foreach($value as $element){
+                        echo '<td>' . $element . '</td>';
+                    }
+                    echo '</tr>';
+                }
+                echo '</table>';
+
+                $result->close();
+                mysqli_close($conn);
+            ?>
+        
+    </body>
+</html>
